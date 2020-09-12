eba696f333bdca6c029039b8f52db8f5c19c3188
Veverica9 <alija.cenaj@gmai.com>
2020-09-12 14:30:26 +0200

    Ubacene prepreke

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <GL/glut.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define TIMER_INTERVAL 20
 #define TIMER_ID 0
@@ -8,10 +9,14 @@
 #define TIMER_INTERVAL2 20
 #define TIMER_ID2 0
 
+#define TIMER_INTERVAL3 20
+#define TIMER_ID3 0
+
 static void on_display(void);
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_timer(int id);
 static void on_timer2(int id);
+static void on_timer3(int id);
 
 float animation_ongoing = 0;
 float animation_parameter = 0;
@@ -19,8 +24,14 @@ float animation_parameter = 0;
 float animation_ongoing2 = 0;
 float animation_parameter2 = 0;
 
+float animation_ongoing3 = 0;
+float animation_parameter3 = 0;
+
 float poz=0;
 float pozs=0;
+
+float x[301];
+float z[301];
 
 int main(int argc, char** argv){
 
@@ -53,6 +64,8 @@ int main(int argc, char** argv){
     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
 
+    for(int i=0;i<300;i++)
+        z[i]=-21-i;
 
     glutMainLoop();
 
@@ -87,6 +100,12 @@ void on_keyboard(unsigned char key, int x, int y) {
                 glutTimerFunc(TIMER_INTERVAL2,on_timer2,TIMER_ID2);
             }else
                 animation_ongoing2=0;
+
+            if(!animation_ongoing3){
+                animation_ongoing3=1;
+                glutTimerFunc(TIMER_INTERVAL3,on_timer3,TIMER_ID3);
+            }else
+                animation_ongoing3=0;
             break;
 
         case 27:
@@ -132,6 +151,26 @@ void on_timer2(int id) {
     glutTimerFunc(TIMER_INTERVAL2,on_timer2,TIMER_ID2);
     }
 }
+//timer za prepreke
+void on_timer3(int id) {
+    if (id != TIMER_ID3) {
+        return;
+    }
+
+    if(animation_parameter3>=2000){
+        animation_parameter3=0;
+        glutPostRedisplay();
+
+    }
+    animation_parameter3 += 1;
+
+    glutPostRedisplay();
+
+    if(animation_ongoing3){
+    glutTimerFunc(TIMER_INTERVAL3,on_timer3,TIMER_ID3);
+    }
+}
+
 
 //crtanje aviona
 void draw_avion(){
@@ -181,7 +220,7 @@ void draw_arena(){
 //shoot
 void draw_shot(){
     glPushMatrix();
-        GLfloat ambient3[] = {0.25,0.35,0.79,0};
+        GLfloat ambient3[] = {1,0.08,0.19,0};
         GLfloat diffuse3[] = {0.3,0.3,0.3,0};
         GLfloat specular3[] = {0.6,0.6,0.6,0};
         GLfloat shininess3 = 10;
@@ -197,6 +236,25 @@ void draw_shot(){
     glPopMatrix();
 }
 
+void draw_preke(float x[],float z[]){
+    GLfloat ambient3[] = {0.25,0.35,0.79,0};
+    GLfloat diffuse3[] = {0.3,0.3,0.3,0};
+    GLfloat specular3[] = {0.6,0.6,0.6,0};
+    GLfloat shininess3 = 10;
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient3);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse3);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular3);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess3);
+
+    for(int i=0;i<300;i++){
+        glPushMatrix();
+            glTranslatef(x[i],0.5,z[i]);
+            glutSolidCube(1);
+        glPopMatrix();
+    }
+}
+
 static void on_display(void){
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -208,6 +266,8 @@ static void on_display(void){
               0, 0, 0,
               0, 1, 0);
 
+    srand(time(NULL));
+
     glPushMatrix();
         glTranslatef(0,0,animation_parameter/10);
         draw_arena();
@@ -216,6 +276,15 @@ static void on_display(void){
     glPushMatrix();
         glTranslatef(poz,0,0);
         draw_avion();
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslatef(0,0,animation_parameter3/10);
+        for(int i=0;i<300;i++){
+            if(animation_parameter3==1)
+                x[i] = rand()%6-3;
+        }
+        draw_preke(x,z);
     glPopMatrix();
 
     if(animation_parameter2==1){
